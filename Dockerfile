b4c27936f98f09e1843042e3559affbe44ec9e0d
traptrip <aletni@gmail.com>
2020-12-07 13:23:31 +0500

    add gunicorn and normal scrolling

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM python:3.8
+FROM python:3.8-slim
 
 COPY . .
 RUN pip install -r requirements.txt
-CMD ["python", "app.py"]
+#CMD ["python", "app.py"]
+CMD gunicorn -w 4 --bind 0.0.0.0:${PORT} app:app
--- app.py
+++ app.py
@@ -1,14 +1,15 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/main')
-@app.route('/front/templates/main_page.html')
 def index():
     return render_template('main_page.html')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True)
